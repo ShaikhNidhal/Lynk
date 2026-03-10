0678aaa8150ab3522e21aad4127015288f8b3fd6
Nidhal Shaikh <nidhal.shaikh@gmail.com>
2026-03-10 07:07:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7140172946-dab17",
+  "appId": "1:888798179565:web:6bbd27e8e8f30340785ce4",
+  "apiKey": "AIzaSyAxFvpdiWV3siubgUhR1LmcepW7txEV3qM",
+  "authDomain": "studio-7140172946-dab17.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "888798179565"
+};
